820d150683bbac6a59a4ec0f28fe6a2186cd769a
sathwik <sky5276@psu.edu>
2024-08-31 08:29:10 -0400

    first commit

---
+++ assignmentworkflow/assignmentworkflow/assignmentworkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentworkflowApp.swift
+//  assignmentworkflow
+//
+//  Created by Owner on 31/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct assignmentworkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
